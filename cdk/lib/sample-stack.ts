7185fd56368d6cb2f92a3350d85a6977b43732b2
Srđan Đukić <srdan.dukic@gmail.com>
2025-09-04 12:54:50 +1200

    Added deploy steps
    Issue(s): None

--- cdk/lib/sample-stack.ts
+++ cdk/lib/sample-stack.ts
@@ -4,32 +4,48 @@ import {Vpc} from 'aws-cdk-lib/aws-ec2'
 import {Construct} from 'constructs'
 import {IpAddresses} from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class SampleStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
 
-    new Repository(this, 'Repo');
+    const repo = new Repository(this, 'Repo');
 
     const vpc = new Vpc(this, 'Vpc', {
       ipAddresses: IpAddresses.cidr('10.0.0.0/16')
     });
 
-    // TODO: Create an ECS cluster
     const cluster = new ecs.Cluster(this, 'EcsCluster', { 
       vpc,
       enableFargateCapacityProviders: true 
     });
 
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition');
+    // Create execution role for ECS Fargate to pull images from ECR
+    const executionRole = new iam.Role(this, 'ExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy')
+      ]
+    });
+
+    // Grant ECR permissions to the execution role
+    repo.grantPull(executionRole);
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition', {
+      executionRole
+    });
 
     taskDefinition.addContainer('Container', {
-      image: ecs.ContainerImage.fromRegistry('nginx'),
+      image: ecs.ContainerImage.fromRegistry(repo.repositoryUri),
       memoryLimitMiB: 512,
+      portMappings: [{
+        containerPort: 3000
+      }],
       cpu: 256
     });
 
-    const service = new ecs.FargateService(this, 'Service', {
+    new ecs.FargateService(this, 'Service', {
       cluster,
       taskDefinition,
       minHealthyPercent: 100
